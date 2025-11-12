2771d6c49455b6909195d5ba7ccb1c28ebde72bb
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-12 14:04:00 +0100

    2025_11_12 MinIO exo2

--- 2025_11_10_MinIO/exos/exo1.py
+++ 2025_11_10_MinIO/exos/exo1.py
@@ -2,6 +2,7 @@ import os
 from lorem_text import lorem
 from minio import Minio
 from minio.commonconfig import CopySource
+from minio.deleteobjects import DeleteObject
 
 
 def generate_file(file_path) -> None:
@@ -13,7 +14,7 @@ def generate_file(file_path) -> None:
 
 def main() -> None:
     # Question 1
-    print("Création d'un client.")
+    print("1. Création d'un client.")
     client = Minio(
         endpoint="localhost:9000",
         access_key="minioadmin",
@@ -23,30 +24,30 @@ def main() -> None:
 
     # Question 2
     bucket_name = "bucket1"
-    print(f"Création du bucket {bucket_name}.")
+    print(f"2.Création du bucket {bucket_name}.")
     if client.bucket_exists(bucket_name=bucket_name):
-        print(f"{bucket_name} already exists.")
+        print(f"\t{bucket_name} already exists.")
     else:
         client.make_bucket(bucket_name=bucket_name)
-        print(f"{bucket_name} maked.")
+        print(f"\t{bucket_name} maked.")
     
     # Question 3
     file_path = "./2025_11_10_MinIO/exos/file1.txt"
-    print(f"Génération du fichier {file_path}")
+    print(f"3. Génération du fichier {file_path}")
     generate_file(file_path=file_path)
 
     # Question 4
     object_name = "file1.txt"
-    print(f"Téléchargement du fichier {file_path} dans le bucket {bucket_name}.")
+    print(f"4.Téléchargement du fichier {file_path} dans le bucket {bucket_name}.")
     client.fput_object(
         bucket_name=bucket_name,
         object_name=object_name,
         file_path=file_path
         )
-    print(f"{file_path} puted in {bucket_name}.")
+    print(f"\t{file_path} puted in {bucket_name}.")
 
     # Question 5
-    print(f"Affichage de la liste des objets du bucket {bucket_name}.")
+    print(f"5. Affichage de la liste des objets du bucket {bucket_name}.")
     for object in client.list_objects(bucket_name=bucket_name):
         print(object.object_name)
 
@@ -56,7 +57,7 @@ def main() -> None:
         object_name=object_name
         )
     object_name_copy = "file1_copy.txt"
-    print(f"Copie de l'objet {object_name} en {object_name_copy}")
+    print(f"6. Copie de l'objet {object_name} en {object_name_copy}")
     client.copy_object(bucket_name=bucket_name, object_name=object_name_copy, source=copy_source)
 
     # Question 7
@@ -65,24 +66,35 @@ def main() -> None:
         object_name=object_name
         )
     object_name_rename = "file2.txt"
-    print(f"Renommage de l'objet {object_name} en {object_name_rename}")
+    print(f"7. Renommage de l'objet {object_name} en {object_name_rename}")
     client.copy_object(bucket_name=bucket_name, object_name=object_name_rename, source=copy_source)
     client.remove_object(bucket_name=bucket_name, object_name=object_name)
 
     # Question 8
-    print(f"Récupération de l'objet {object_name_rename}")
+    print(f"8. Récupération de l'objet {object_name_rename}")
+    file_path_get = "./2025_11_10_MinIO/exos/file_get.txt"
+    client.fget_object(bucket_name=bucket_name, object_name=object_name_rename, file_path=file_path_get)
+
     response = client.get_object(bucket_name=bucket_name, object_name=object_name_rename)
-    print(response.data)
+    print(f"{response.data}")
     response.close()
     response.release_conn()
 
     # Question 9
-    print(f"Suppression des objets {object_name_copy} et {object_name_rename}.")
-    client.remove_objects(bucket_name=bucket_name, delete_object_list=[object_name_rename, object_name_copy])
+    print(f"9. Suppression des objets {object_name_copy} et {object_name_rename}. Suppression du bicket {bucket_name}.")
+    delete_object_list = [
+        DeleteObject(name=object_name_rename),
+        DeleteObject(name=object_name_copy)
+        ]
+    errors = client.remove_objects(bucket_name=bucket_name, delete_object_list=delete_object_list)
+    for error in errors:
+        print("error occured when deleting object", error)
+    client.remove_bucket(bucket_name=bucket_name)
 
     # Question 10
-    print(f"Suppression du fichier local {file_path}.")
+    print(f"10. Suppression du fichier local {file_path}.")
     os.remove(file_path)
+    os.remove(file_path_get)
 
 
 
---
+++ 2025_11_10_MinIO/exos/exo2.py
@@ -0,0 +1,149 @@
+import csv
+from dataclasses import dataclass
+from minio import Minio
+
+
+@dataclass
+class Customer:
+    index: int
+    customer_id: str
+    first_name: str
+    last_name: str
+    company: str
+    city: str
+    country: str
+    phone1: str
+    phone2: str
+    email: str
+    subscription_data: str
+    website: str
+
+
+    def values(self) -> list[str]:
+        return [
+            str(self.index),
+            self.customer_id,
+            self.first_name,
+            self.last_name,
+            self.company,
+            self.city,
+            self.country,
+            self.phone1,
+            self.phone2,
+            self.email,
+            self.subscription_data,
+            self.website
+            ]
+
+
+def load_csv(file_path) -> dict[int, Customer]:
+    customers: dict[int, Customer] = {}
+    with open(file_path, 'r', newline='', encoding="utf-8") as csvfile:
+        fieldnames = ["Index", "Customer Id", "First Name", "Last Name", "Company", "City", "Country", "Phone 1", "Phone 2", "Email", "Subscription Date", "Website"]
+        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        for row in reader:
+            index: str = row["Index"]
+            if index.isdigit():
+                index=int(row["Index"])
+            customer: Customer = Customer(
+                index=index,
+                customer_id=row["Customer Id"],
+                first_name=row["First Name"],
+                last_name=row["Last Name"],
+                company=row["Company"],
+                city=row["City"],
+                country=row["Country"],
+                phone1=row["Phone 1"],
+                phone2=row["Phone 2"],
+                email=row["Email"],
+                subscription_data=row["Subscription Date"],
+                website=row["Website"]
+                )
+            customers.update({index: customer})
+    # remove header
+    customers.pop("Index")
+    return customers
+
+
+def write_csv(file_path, customers: dict[int, Customer]) -> None:
+    with open(file_path, 'w', newline='', encoding="utf-8") as csvfile:
+        fieldnames = ["Index", "Customer Id", "First Name", "Last Name", "Company", "City", "Country", "Phone 1", "Phone 2", "Email", "Subscription Date", "Website"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for customer in customers.values():
+            writer.writerow({
+                "Index": customer.index,
+                "Customer Id": customer.customer_id,
+                "First Name": customer.first_name,
+                "Last Name": customer.last_name,
+                "Company": customer.company,
+                "City": customer.city,
+                "Country": customer.country,
+                "Phone 1": customer.phone1,
+                "Phone 2": customer.phone2,
+                "Email": customer.email,
+                "Subscription Date": customer.subscription_data,
+                "Website": customer.website
+            })
+
+
+def write_txt(file_path, customers: dict[int, Customer]) -> None:
+    with open(file_path, 'w', encoding="utf-8") as txtfile:
+        fieldnames = ["Index", "Customer Id", "First Name", "Last Name", "Company", "City", "Country", "Phone 1", "Phone 2", "Email", "Subscription Date", "Website"]
+        txtfile.write(",".join(fieldnames) + "\n")
+        for customer in customers.values():
+            txtfile.write(",".join(customer.values())  + "\n")
+
+
+def filter_csv(customers: dict[int, Customer]) -> tuple[dict[int, Customer]]:
+    customers_am: dict[int, Customer] = {}
+    customers_nz: dict[int, Customer] = {}
+    for index, customer in customers.items():
+        if customer.country[0].lower() in "abcdefghijklm":
+            customers_am.update({index: customer})
+        if customer.country[0].lower() in "nopqrstuvwxyz":
+            customers_nz.update({index: customer})
+    return customers_am, customers_nz
+
+
+def make_bucket(client: Minio, bucket_name: str) -> None:
+    if client.bucket_exists(bucket_name=bucket_name):
+        print(f"{bucket_name} already exists.")
+    else:
+        client.make_bucket(bucket_name=bucket_name)
+        print(f"{bucket_name} is created.")
+
+
+def main() -> None:
+    endpoint = "localhost:9000"
+    access_key = "minioadmin"
+    secret_key = "minioadmin"
+    secure = False
+    client: Minio = Minio(
+        endpoint=endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=secure
+    )
+
+    bucket_name_am="customers-am-bucket"
+    bucket_name_nz="customers-nz-bucket"
+    make_bucket(client=client, bucket_name=bucket_name_am)
+    make_bucket(client=client, bucket_name=bucket_name_nz)
+
+    file_path = "./2025_11_10_MinIO/exos/customers.csv"
+    customers = load_csv(file_path=file_path)
+    customers_am, customers_nz = filter_csv(customers=customers)
+    file_path_am = "./2025_11_10_MinIO/exos/customers_am.txt"
+    file_path_nz = "./2025_11_10_MinIO/exos/customers_nz.csv"
+    write_txt(file_path=file_path_am, customers=customers_am)
+    write_csv(file_path=file_path_nz, customers=customers_nz)
+
+    object_name_am = "customers_am.txt"
+    object_name_nz = "customers_nz.csv"
+    client.fput_object(bucket_name=bucket_name_am, object_name=object_name_am, file_path=file_path_am)
+    client.fput_object(bucket_name=bucket_name_nz, object_name=object_name_nz, file_path=file_path_nz)
+
+
+if __name__ == "__main__":
+    main()

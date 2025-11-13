3cb19d8b4acc24aa7cd444398b45f562f551fa0f
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-13 13:40:32 +0100

    MinIO exo3 bugfix

--- 2025_11_10_MinIO/exos/exo3/exo3.py
+++ 2025_11_10_MinIO/exos/exo3/exo3.py
@@ -1,27 +1,29 @@
-import json
+from typing import Callable
 from minio import Minio
+from minio.error import S3Error
 
 
 def list_all_objects(client: Minio):
-    print(f"Client {client.__repr__()}")
+    # print(f"Client {client.__repr__()}")
     for bucket in client.list_buckets():
-        print(f"Bucket : {bucket.name}")
+        # print(f"Bucket : {bucket.name}")
         for object in client.list_objects(bucket_name=bucket.name):
-            print(f"Objet : {object.object_name}")
+            pass
+            # print(f"Objet : {object.object_name}")
+
+
+def test(description: str, callable: Callable, *args, **kwargs):
+    print(description)
+    try:
+        callable(*args, **kwargs)
+        print("Succes")
+    except S3Error:
+        print("Echec")
 
 
 def main() -> None:
     endpoint = "localhost:9000"
     secure = False
-
-    # access_key = "minioadmin"
-    # secret_key = "minioadmin"
-    # client: Minio = Minio(
-    #     endpoint=endpoint,
-    #     access_key=access_key,
-    #     secret_key=secret_key,
-    #     secure=secure
-    # )
 
     access_key_admin = "minio-admin"
     secret_key_admin = "minioadmin"
@@ -53,15 +55,48 @@ def main() -> None:
     bucket_admin = "admin-bucket"
     bucket_manager = "manager-bucket"
     bucket_client = "client-bucket"
-    list_all_objects(client=client_admin)
-    list_all_objects(client=client_manager)
-    list_all_objects(client=client_client)
+    test(description="ListBucket Admin", callable=list_all_objects, client=client_admin)
+    test(description="ListBucket Manager", callable=list_all_objects, client=client_manager)
+    test(description="ListBucket client", callable=list_all_objects, client=client_client)
 
     object_name = "customers.csv"
     file_path = "./2025_11_10_MinIO/exos/exo3/file.csv"
-    client_admin.fput_object(bucket_name=bucket_admin, object_name=object_name, file_path=file_path)
-    client_admin.fput_object(bucket_name=bucket_manager, object_name=object_name, file_path=file_path)
-    client_admin.fput_object(bucket_name=bucket_client, object_name=object_name, file_path=file_path)
+    test(description="PutObject admin/admin", callable=client_admin.fput_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path)
+    test(description="PutObject admin/manager", callable=client_admin.fput_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path)
+    test(description="PutObject admin/client", callable=client_admin.fput_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path)
+
+    test(description="PutObject manager/admin", callable=client_manager.fput_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path)
+    test(description="PutObject manager/manager", callable=client_manager.fput_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path)
+    test(description="PutObject manager/client", callable=client_manager.fput_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path)
+
+    test(description="PutObject client/admin", callable=client_client.fput_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path)
+    test(description="PutObject client/manager", callable=client_client.fput_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path)
+    test(description="PutObject client/client", callable=client_client.fput_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path)
+
+    file_path_get = "./2025_11_10_MinIO/exos/exo3/file_get.csv"
+    test(description="GetObject admin/admin", callable=client_admin.fget_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject admin/manager", callable=client_admin.fget_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject admin/client", callable=client_admin.fget_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path_get)
+
+    test(description="GetObject manager/admin", callable=client_manager.fget_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject manager/manager", callable=client_manager.fget_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject manager/client", callable=client_manager.fget_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path_get)
+
+    test(description="GetObject client/admin", callable=client_client.fget_object, bucket_name=bucket_admin, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject client/manager", callable=client_client.fget_object, bucket_name=bucket_manager, object_name=object_name, file_path=file_path_get)
+    test(description="GetObject client/client", callable=client_client.fget_object, bucket_name=bucket_client, object_name=object_name, file_path=file_path_get)
+
+    test(description="DeleteObject admin/admin", callable=client_admin.remove_object, bucket_name=bucket_admin, object_name=object_name)
+    test(description="DeleteObject admin/manager", callable=client_admin.remove_object, bucket_name=bucket_manager, object_name=object_name)
+    test(description="DeleteObject admin/client", callable=client_admin.remove_object, bucket_name=bucket_client, object_name=object_name)
+
+    test(description="DeleteObject manager/admin", callable=client_manager.remove_object, bucket_name=bucket_admin, object_name=object_name)
+    test(description="DeleteObject manager/manager", callable=client_manager.remove_object, bucket_name=bucket_manager, object_name=object_name)
+    test(description="DeleteObject manager/client", callable=client_manager.remove_object, bucket_name=bucket_client, object_name=object_name)
+
+    test(description="DeleteObject client/admin", callable=client_client.remove_object, bucket_name=bucket_admin, object_name=object_name)
+    test(description="DeleteObject client/manager", callable=client_client.remove_object, bucket_name=bucket_manager, object_name=object_name)
+    test(description="DeleteObject client/client", callable=client_client.remove_object, bucket_name=bucket_client, object_name=object_name)
 
 
 if __name__ == "__main__":

b068b15509e33f99381d5c6f615baf5df40006bc
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-13 11:48:43 +0100

    MinIO Exo3

---
+++ 2025_11_10_MinIO/exos/exo3.md
@@ -0,0 +1,56 @@
+# Exercice 3
+
+## Création des buckets.
+
+```bash
+mc mb local/admin-bucket
+mc mb local/manager-bucket
+mc mb local/client-bucket
+mc ls local
+```
+
+Buckets |
+-|
+admin-bucket/ |
+client-bucket/ |
+manager-bucket/ |
+
+## Création des policies.
+
+```bash
+mc admin policy create local admin-policy "./2025_11_10_MinIO/exos/admin-policy.json"
+mc admin policy create local manager-policy "./2025_11_10_MinIO/exos/manager-policy.json"
+mc admin policy create local client-policy "./2025_11_10_MinIO/exos/client-policy.json"
+mc admin policy list local
+```
+
+## Création des comptes admin, manager et user
+
+```bash
+mc admin user add local minio-admin "minioadmin"
+mc admin user add local minio-manager "miniomanager"
+mc admin user add local minio-client "minioclient"
+mc admin user list local
+```
+## Attache des policies.
+
+```bash
+mc admin policy attach local admin-policy --user minio-admin
+mc admin policy attach local manager-policy --user minio-manager
+mc admin policy attach local client-policy --user minio-client
+mc admin user list local
+```
+
+? | User | Policy
+-|-|-
+enabled | minio-admin | admin-policy
+enabled | minio-client | client-policy
+enabled | minio-manager | manager-policy
+
+## Création des alias
+
+```bash
+mc alias set minio-admin http://localhost:9000 minio-admin "minioadmin"
+mc alias set minio-manager http://localhost:9000 minio-manager "miniomanager"
+mc alias set minio-client http://localhost:9000 minio-client "minioclient"
+```
---
+++ 2025_11_10_MinIO/exos/exo3.py
@@ -0,0 +1,68 @@
+import json
+from minio import Minio
+
+
+def list_all_objects(client: Minio):
+    print(f"Client {client.__repr__()}")
+    for bucket in client.list_buckets():
+        print(f"Bucket : {bucket.name}")
+        for object in client.list_objects(bucket_name=bucket.name):
+            print(f"Objet : {object.object_name}")
+
+
+def main() -> None:
+    endpoint = "localhost:9000"
+    secure = False
+
+    # access_key = "minioadmin"
+    # secret_key = "minioadmin"
+    # client: Minio = Minio(
+    #     endpoint=endpoint,
+    #     access_key=access_key,
+    #     secret_key=secret_key,
+    #     secure=secure
+    # )
+
+    access_key_admin = "minio-admin"
+    secret_key_admin = "minioadmin"
+    client_admin: Minio = Minio(
+        endpoint=endpoint,
+        access_key=access_key_admin,
+        secret_key=secret_key_admin,
+        secure=secure
+    )
+
+    access_key_manager = "minio-manager"
+    secret_key_manager = "miniomanager"
+    client_manager: Minio = Minio(
+        endpoint=endpoint,
+        access_key=access_key_manager,
+        secret_key=secret_key_manager,
+        secure=secure
+    )
+
+    access_key_client = "minio-client"
+    secret_key_client = "minioclient"
+    client_client: Minio = Minio(
+        endpoint=endpoint,
+        access_key=access_key_client,
+        secret_key=secret_key_client,
+        secure=secure
+    )
+
+    bucket_admin = "admin-bucket"
+    bucket_manager = "manager-bucket"
+    bucket_client = "client-bucket"
+    list_all_objects(client=client_admin)
+    list_all_objects(client=client_manager)
+    list_all_objects(client=client_client)
+
+    object_name = "customers.csv"
+    file_path = "./2025_11_10_MinIO/exos/customers.csv"
+    client_admin.fput_object(bucket_name=bucket_admin, object_name=object_name, file_path=file_path)
+    client_admin.fput_object(bucket_name=bucket_manager, object_name=object_name, file_path=file_path)
+    client_admin.fput_object(bucket_name=bucket_client, object_name=object_name, file_path=file_path)
+
+
+if __name__ == "__main__":
+    main()

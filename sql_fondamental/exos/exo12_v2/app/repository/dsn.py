6075617756eb7d4a27de792477d28b042cdfabe1
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-11-25 16:55:30 +0100

    SQL Fondamental exo12 dsn.py

--- sql_fondamental/exos/exo12_v2/app/repository/dsn.py
+++ sql_fondamental/exos/exo12_v2/app/repository/dsn.py
@@ -1,1 +1,15 @@
+from pydantic_settings import BaseSettings
+
 DSN = "dbname=mydb user=admin password=admin host=pgdb port=5432"
+
+
+class Settings(BaseSettings):
+    dbname: str = "mydb"
+    user: str = "admin"
+    password: str = "admin"
+    host: str = "pgdb"
+    port: int = 5432
+
+
+if __name__ == "__main__":
+    print(Settings())

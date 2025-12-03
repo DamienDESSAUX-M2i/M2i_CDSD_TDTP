de82d3b3c40d1905e0ebba5ce259d351af634000
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-12-03 09:26:33 +0100

    ETL exo1 md + pdf

--- etl/exos/exo1.py
+++ etl/exos/exo1.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
+from markdown_pdf import MarkdownPdf, Section
 
 file_dir = Path(__file__).parent.resolve()
 
 content: list[str] = []
-content.append(f"Rapport {datetime.today().__format__('%Y-%m-%d %H:%M:%S')}")
+content.append(f"# Rapport {datetime.today().__format__('%Y-%m-%d %H:%M:%S')}")
 content.append("")
 
 # Question 1
@@ -15,8 +16,8 @@ df = pd.read_csv(path_ventes_csv)
 
 # Question 2
 df["montant_total"] = df["quantite"] * df["prix_unitaire"]
-content.append("Données")
-content.append(str(df))
+content.append("## Données")
+content.append(str(df.to_markdown(index=False)))
 content.append("")
 
 # Question 3
@@ -27,8 +28,8 @@ total_vendeur = (
     .reset_index()
     .rename(columns={"montant_total": "total_ventes"})
 )
-content.append("Total des ventes par vendeur")
-content.append(str(total_vendeur))
+content.append("## Total des ventes par vendeur")
+content.append(str(total_vendeur.to_markdown(index=False)))
 content.append("")
 
 # Question 4
@@ -39,20 +40,26 @@ total_produit = (
     .reset_index()
     .rename(columns={"montant_total": "total_ventes"})
 )
-content.append("Total des ventes par produit")
-content.append(str(total_produit))
+content.append("## Total des ventes par produit")
+content.append(str(total_produit.to_markdown(index=False)))
 content.append("")
 
 # Question 5
 top3_montant_total = df.sort_values("montant_total", ascending=False).head(3)
-content.append("Top 3 des ventes")
-content.append(str(top3_montant_total))
+content.append("## Top 3 des ventes")
+content.append(str(top3_montant_total.to_markdown(index=False)))
 content.append("")
 
 # Question 6
 path_ventes_anelysees_csv = file_dir.joinpath("ventes_analysees.csv")
 df.to_csv(path_ventes_anelysees_csv, index=False)
 
-path_report_txt = file_dir.joinpath("report.txt")
-with open(path_report_txt, "wt", encoding="utf-8") as report:
+path_report_md = file_dir.joinpath("report.md")
+path_report_pdf = file_dir.joinpath("report.pdf")
+with open(path_report_md, "wt", encoding="utf-8") as report:
     report.writelines("\n".join(content))
+
+    pdf = MarkdownPdf()
+    pdf.meta["title"] = "Report"
+    pdf.add_section(Section("\n".join(content), toc=False))
+    pdf.save(path_report_pdf)
---
+++ etl/exos/report.md
@@ -0,0 +1,31 @@
+# Rapport 2025-12-03 09:16:26
+
+## Données
+| date       | produit   |   quantite |   prix_unitaire | vendeur   |   montant_total |
+|:-----------|:----------|-----------:|----------------:|:----------|----------------:|
+| 2024-01-15 | Laptop    |          2 |          899.99 | Alice     |         1799.98 |
+| 2024-01-15 | Souris    |          5 |           29.99 | Bob       |          149.95 |
+| 2024-01-16 | Clavier   |          3 |           79.99 | Alice     |          239.97 |
+| 2024-01-16 | Laptop    |          1 |          899.99 | Charlie   |          899.99 |
+| 2024-01-17 | Souris    |         10 |           29.99 | Alice     |          299.9  |
+
+## Total des ventes par vendeur
+| vendeur   |   total_ventes |
+|:----------|---------------:|
+| Alice     |        2339.85 |
+| Charlie   |         899.99 |
+| Bob       |         149.95 |
+
+## Total des ventes par produit
+| produit   |   total_ventes |
+|:----------|---------------:|
+| Laptop    |        2699.97 |
+| Souris    |         449.85 |
+| Clavier   |         239.97 |
+
+## Top 3 des ventes
+| date       | produit   |   quantite |   prix_unitaire | vendeur   |   montant_total |
+|:-----------|:----------|-----------:|----------------:|:----------|----------------:|
+| 2024-01-15 | Laptop    |          2 |          899.99 | Alice     |         1799.98 |
+| 2024-01-16 | Laptop    |          1 |          899.99 | Charlie   |          899.99 |
+| 2024-01-17 | Souris    |         10 |           29.99 | Alice     |          299.9  |

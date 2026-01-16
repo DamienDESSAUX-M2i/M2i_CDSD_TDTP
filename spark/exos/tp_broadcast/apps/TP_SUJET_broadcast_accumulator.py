9f4b2e44687b4f68a153ded04cdfd3bde2575adb
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2026-01-16 10:42:18 +0100

    Spark TP broadcast et accumulator.

--- spark/exos/tp_broadcast/apps/TP_SUJET_broadcast_accumulator.py
+++ spark/exos/tp_broadcast/apps/TP_SUJET_broadcast_accumulator.py
@@ -157,6 +157,7 @@ import time
 from typing import Tuple
 
 import pyspark.sql.functions as spark_func
+from pyspark.accumulators import AccumulatorParam
 from pyspark.sql import SparkSession
 from pyspark.sql.types import (
     DoubleType,
@@ -283,8 +284,6 @@ clients_enriched2 = clients_enriched1.withColumn(
     "client_id", transformClientId(spark_func.col("client_id"))
 )
 
-t0 = time.time()
-
 clients_dict = {}
 for row in clients_enriched2.select(
     ["client_id", "nom", "prenom", "segment"]
@@ -317,20 +316,22 @@ schema = StructType(
 
 enrichedWithClient = spark_func.udf(enriched_with_client, schema)
 
+t1 = time.time()
+
 achats_enriched1 = achats.withColumn(
     "client_info", enrichedWithClient(spark_func.col("id_client"))
 )
 
-t1 = time.time()
+t2 = time.time()
 
-print(f"\nAggregation WITH broadcast: {t1 - t0}")
+print(f"\nAggregation WITH broadcast: {t2 - t1}")
 
 print("\nAchats enriched:")
 achats_enriched1.show(5)
 
 # 3.4 Comparer le temps d'exécution avec une jointure classique
 
-t2 = time.time()
+t3 = time.time()
 
 achats_clients = achats.join(
     other=clients_enriched2,
@@ -338,9 +339,9 @@ achats_clients = achats.join(
     how="left",
 )
 
-t3 = time.time()
+t4 = time.time()
 
-print(f"\nAggregation WITHOUT broadcast: {t3 - t2}")
+print(f"\nAggregation WITHOUT broadcast: {t4 - t3}")
 
 print("\nAchats clients:")
 achats_clients.show(5)
@@ -417,11 +418,45 @@ print("=" * 70)
 # On veut collecter la liste des clients inconnus et les montants invalides
 # pour un rapport d'erreurs.
 
+
 # 5.1 Créer un AccumulatorParam personnalisé pour collecter des listes
+class ListCollector(AccumulatorParam):
+    def zero(self, v):
+        return []
+
+    def addInPlace(self, variable: list, value: str):
+        variable.append(value)
+        return variable
+
+
 # 5.2 Collecter :
 #     - La liste des id_client inconnus
 #     - La liste des montants invalides (valeur brute)
+list_montants_invalides = sc.accumulator([], ListCollector())
+list_clients_inconnus = sc.accumulator([], ListCollector())
+
+
+def parsing_row2(row):
+    # Montant invalide
+    try:
+        montant = float(row["montant"])
+        if montant < 0:
+            list_montants_invalides.add(row["montant"])
+    except:
+        list_montants_invalides.add(row["montant"])
+
+    # Client inconnu
+    if not row["client_id"]:
+        list_clients_inconnus.add(row["client_id"])
+    return row
+
+
+achats_clients.foreach(parsing_row2)
+
 # 5.3 Afficher le rapport d'erreurs
+print("\nParsing stats:")
+print("Montants invalides", list_montants_invalides.value)
+print("Clients inconnus", list_clients_inconnus.value)
 
 # -------------------------------------------------------------
 # EXERCICE 6 : ANALYSE COMPLÈTE
@@ -430,8 +465,135 @@ print("\n" + "=" * 70)
 print("EXERCICE 6 : ANALYSE COMPLÈTE")
 print("=" * 70)
 
-# TODO: Votre code ici
+# Réaliser une analyse complète des ventes en utilisant broadcast ET accumulator.
 
+# 6.1 Calculer pour chaque segment de client :
+#     - Nombre de transactions valides
+#     - Montant total des achats
+#     - Montant moyen par transaction
+# 6.2 Pendant le traitement, collecter via accumulators :
+#     - Nombre total de transactions traitées
+#     - Nombre d'erreurs par type
+#     - Montant total des ventes valides
+
+senior_transaction_valide = sc.accumulator(0)
+senior_montant_total = sc.accumulator(0)
+b2c_transaction_valide = sc.accumulator(0)
+b2c_montant_total = sc.accumulator(0)
+etudiant_transaction_valide = sc.accumulator(0)
+etudiant_montant_total = sc.accumulator(0)
+b2b_transaction_valide = sc.accumulator(0)
+b2b_montant_total = sc.accumulator(0)
+vip_transaction_valide = sc.accumulator(0)
+vip_montant_total = sc.accumulator(0)
+
+err_transaction_invalide = sc.accumulator(0)
+err_montants_invalides = sc.accumulator(0)
+err_montants_negatifs = sc.accumulator(0)
+err_montants_vides = sc.accumulator(0)
+err_clients_inconnus = sc.accumulator(0)
+
+MAP_ACC = {
+    "Sénior": {
+        "transactions_valides": senior_transaction_valide,
+        "montant_total": senior_montant_total,
+    },
+    "B2C": {
+        "transactions_valides": b2c_transaction_valide,
+        "montant_total": b2c_montant_total,
+    },
+    "Etudiant": {
+        "transactions_valides": etudiant_transaction_valide,
+        "montant_total": etudiant_montant_total,
+    },
+    "B2B": {
+        "transactions_valides": b2b_transaction_valide,
+        "montant_total": b2b_montant_total,
+    },
+    "VIP": {
+        "transactions_valides": vip_transaction_valide,
+        "montant_total": vip_montant_total,
+    },
+}
+
+
+def parsing_row3(row):
+    acc_dict = MAP_ACC[row["segment"]]
+    # Client inconnu
+    if not row["client_id"]:
+        err_transaction_invalide.add(1)
+        err_clients_inconnus.add(1)
+        return None
+    # Montant invalide
+    if not row["montant"]:
+        err_transaction_invalide.add(1)
+        err_montants_invalides.add(1)
+        err_montants_vides.add(1)
+        return None
+    try:
+        montant = float(row["montant"])
+        if montant >= 0:
+            acc_dict["montant_total"].add(montant)
+            acc_dict["transactions_valides"].add(1)
+        else:
+            err_transaction_invalide.add(1)
+            err_montants_invalides.add(1)
+            err_montants_negatifs.add(1)
+            return None
+    except:
+        err_transaction_invalide.add(1)
+        err_montants_invalides.add(1)
+        return None
+    return row
+
+
+t5 = time.time()
+
+achats_clients.foreach(parsing_row3)
+
+t6 = time.time()
+
+# 6.3 Afficher :
+#     - Les résultats par segment
+#     - Le taux d'erreur global
+#     - Le rapport qualité des données
+print("\nRésultats par segment")
+for segment in MAP_ACC.keys():
+    print(" \nSegment:", segment)
+    print(
+        " Nombre transactions valides", MAP_ACC[segment]["transactions_valides"].value
+    )
+    print(" Montant total achat", MAP_ACC[segment]["montant_total"].value)
+    print(
+        " Montant moyen par transaction",
+        MAP_ACC[segment]["montant_total"].value
+        / MAP_ACC[segment]["transactions_valides"].value,
+    )
+
+nb_transactions_valides = sum(
+    [MAP_ACC[segment]["transactions_valides"].value for segment in MAP_ACC.keys()]
+)
+montant_total = sum(
+    [MAP_ACC[segment]["montant_total"].value for segment in MAP_ACC.keys()]
+)
+
+print("\nRapport Qualité")
+print(" Montant total transaction valides: ", montant_total)
+print(" Nombre de transactions valides: ", nb_transactions_valides)
+print(" Nombre transactions invalides: ", err_transaction_invalide.value)
+print(
+    " Taux d'erreur (en %): ",
+    round(
+        err_transaction_invalide.value
+        / (nb_transactions_valides + err_transaction_invalide.value)
+        * 100,
+        2,
+    ),
+)
+print(" Nombre montants invalides: ", err_montants_invalides.value)
+print("   Nombre montants négatifs: ", err_montants_negatifs.value)
+print("   Nombre montants vides: ", err_montants_vides.value)
+print(" Nombre clients inconnus: ", err_clients_inconnus.value)
 
 # -------------------------------------------------------------
 # EXERCICE 7 : COMPARAISON DE PERFORMANCE
@@ -440,8 +602,42 @@ print("\n" + "=" * 70)
 print("EXERCICE 7 : COMPARAISON DE PERFORMANCE")
 print("=" * 70)
 
-# TODO: Votre code ici
+# Comparer les approches avec et sans optimisation.
 
+# 7.1 SANS optimisation :
+#     - Jointure classique entre achats et clients
+#     - Filtrage des erreurs avec plusieurs passes
+t7 = time.time()
+
+achats_clients_cleaned = achats_clients.withColumn(
+    "montant", spark_func.col("montant").cast(DoubleType())
+).filter(spark_func.col("montant") > 0)
+achats_clients_cleaned.groupBy("segment").agg(
+    spark_func.sum("montant").alias("montant_total"),
+    spark_func.avg("montant").alias("avg_montant"),
+).show()
+
+t8 = time.time()
+
+# 7.2 AVEC optimisation :
+#     - Broadcast pour la jointure
+#     - Accumulators pour le comptage d'erreurs
+
+# 7.3 Mesurer et comparer les temps d'exécution
+time_with_optimisation = t1 - t2 + t6 - t5
+time_without_optimisation = t4 - t3 + t8 - t7
+print("\nComparison")
+print(" AVEC optimisation: ", t1 - t2 + t6 - t5)
+print(" SANS optimisation: ", t4 - t3 + t8 - t7)
+print(
+    " gain (en %): ",
+    round(
+        (time_without_optimisation - time_with_optimisation)
+        / time_without_optimisation
+        * 100,
+        2,
+    ),
+)
 
 # Fermeture
 spark.stop()
